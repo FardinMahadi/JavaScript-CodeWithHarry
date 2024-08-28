f2c5c3c61a5fedfe08be0051f9f708b10f8adb59
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-08-28 19:30:24 +0600

    done

--- 54/main.js
+++ 54/main.js
@@ -1,6 +1,18 @@
+let promise = new Promise(function (resolve, reject) {
+  alert("Hello");
+  resolve(56);
+});
+
 console.log("Hello");
 setTimeout(function () {
-  console.log("Hello");
-}, 1000);
+  console.log("Hello Two in 2 seconds");
+}, 2000);
 
 console.log("My name is " + "Fardin");
+console.log(promise);
+
+// fetch googl.com homepage ==> cosole.log("google.com homepage done")
+// fetch data from the data api
+// fetch pitcures from the server
+// print downloading
+// rest of the script
