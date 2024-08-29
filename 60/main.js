23bf443b0855fd218715cc76ccf736f158ff69ae
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-08-29 20:15:08 +0600

    commit

---
+++ 60/main.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+  console.log("Hacking wifi.......");
+  console.log("Please wait.......");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Please wait......");
+  console.log("Fetching username and password...");
+}, 2000);
+
+setTimeout(() => {
+  console.log("Please wait......");
+  console.log("Fetching username and password...");
+}, 2000);
