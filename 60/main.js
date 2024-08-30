98d64aaaa596d8e08d0689c562bd1412750b0c42
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-08-30 10:58:00 +0600

    done

--- 60/main.js
+++ 60/main.js
@@ -3,6 +3,18 @@ setTimeout(() => {
   console.log("Please wait.......");
 }, 1000);
 
+try {
+  setTimeout(() => {
+    try {
+      console.log(fardin);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 100);
+} catch (err) {
+  console.log("Balle balle " + err);
+}
+
 setTimeout(() => {
   console.log("Please wait......");
   console.log("Fetching username and password...");
@@ -10,5 +22,10 @@ setTimeout(() => {
 
 setTimeout(() => {
   console.log("Please wait......");
-  console.log("Fetching username and password...");
-}, 2000);
+  console.log("Fetching fardin's facebook id'...");
+}, 3000);
+
+setTimeout(() => {
+  console.log("Please wait......");
+  console.log("username and password hacked...");
+}, 4000);
